3e1e05873a78d5de7d6ec2b6e7be6dfdc9c27e5e
cszcszczs <lasbolas01@gmail.com>
2026-09-22 00:12:17 -0500

    <feat> add the login screen

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,17 @@
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AppstackParamList } from '../types/navigation';
+import { LoginScreen } from '../screens/LoginScreen';
+
+const Stack = createNativeStackNavigator<AppstackParamList>();
+
+export function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='Login' component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
